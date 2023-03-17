d5c544a8c8486e45ccca53875d6f559a37602c81
Shreyas Chandra Sekhar <cshreyastech@gmail.com>
2023-03-17 07:41:56 -0700

    Working on OpenCV socket programming

--- OpenCV/src/image_sender.cpp
+++ OpenCV/src/image_sender.cpp
@@ -21,49 +21,62 @@ void error(const char *msg)
 
 int main(int argc, char *argv[])
 {
-    int sockfd, portno, n;
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
+  int sockfd, portno, n;
+  struct sockaddr_in serv_addr;
+  struct hostent *server;
 
-    char buffer[256];
-    // if (argc < 3) {
-    //    fprintf(stderr,"usage %s hostname port\n", argv[0]);
-    //    exit(0);
-    // }
-    portno = atoi("8080");
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
-        error("ERROR opening socket");
-    server = gethostbyname("127.0.0.1");
-    if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host\n");
-        exit(0);
-    }
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, 
-         (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
-    serv_addr.sin_port = htons(portno);
-    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
-        error("ERROR connecting");
+  char buffer[256];
+  // if (argc < 3) {
+  //    fprintf(stderr,"usage %s hostname port\n", argv[0]);
+  //    exit(0);
+  // }
+  portno = atoi("8080");
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd < 0) 
+      error("ERROR opening socket");
+  server = gethostbyname("127.0.0.1");
+  if (server == NULL) {
+      fprintf(stderr,"ERROR, no such host\n");
+      exit(0);
+  }
+  bzero((char *) &serv_addr, sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+  bcopy((char *)server->h_addr, 
+       (char *)&serv_addr.sin_addr.s_addr,
+       server->h_length);
+  serv_addr.sin_port = htons(portno);
+  if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+      error("ERROR connecting");
 
-    Mat image;
-    // image = imread(argv[3], CV_LOAD_IMAGE_COLOR);
-    image = imread("../boyGray.jpg", IMREAD_COLOR);
-    if(! image.data )                              // Check for invalid input
-    {
-        cout <<  "Could not open or find the image" << std::endl ;
-        return -1;
-    }
+  Mat image;
+  // image = imread(argv[3], CV_LOAD_IMAGE_COLOR);
+  image = imread("../boyGray.jpg", IMREAD_COLOR);
+  if(! image.data )                              // Check for invalid input
+  {
+    cout <<  "Could not open or find the image" << std::endl ;
+    return -1;
+  }
 
-    n = write(sockfd,image.data,image.total()*image.channels());
-    if (n < 0) 
-         error("ERROR writing to socket");
-    close(sockfd);
+  Mat frame;
+  // frame = (frame.reshape(0,1)); // to make it continuous
 
-    namedWindow( "Client", WINDOW_AUTOSIZE );// Create a window for display.
-    imshow( "Client", image );  
-    waitKey(0);
-    return 0;
+  int  imgSize = frame.total()*frame.elemSize();
+
+  int bytes = -1;
+  // Send data here
+  bytes = send(sockfd, frame.data, imgSize, 0);
+  if (n < 0) 
+   error("ERROR writing to socket");
+
+  // n = write(sockfd,image.data,image.total()*image.channels());
+  // if (n < 0) 
+  //   error("ERROR writing to socket");
+
+  
+  close(sockfd);
+
+  namedWindow( "Client", WINDOW_AUTOSIZE );// Create a window for display.
+  imshow( "Client", image );  
+  waitKey(0);
+  return 0;
 }
---
+++ OpenCV/src/toy/sample_client.cpp
@@ -0,0 +1,51 @@
+#include "toy/socket_client.hpp"
+
+#include <memory> // unique_ptr
+#include <cstdlib> // atoi
+
+void GenerateImage(int rows, int cols, cv::Mat& image) {
+  int lol = 0;
+  image = cv::Mat::zeros(rows, cols, CV_8UC3);
+  for (int row = 0; row < rows; ++row) {
+    for (int col = 0; col < cols; ++col) {
+      int idk = (lol % 23 == 0) ? rand() % 255 : 35;
+      image.at<cv::Vec3b>(row, col)[0] = idk;
+      image.at<cv::Vec3b>(row, col)[1] = idk;
+      image.at<cv::Vec3b>(row, col)[2] = idk;
+      lol += 7;
+    }
+  }
+}
+
+void AssertCond(bool assert_cond, const char* fail_msg) {
+  if (!assert_cond) {
+    printf("Error: %s\nUsage: ./pic-client <port> <rows> <cols>\n", fail_msg);
+    exit(1);
+  }
+}
+
+void ParseArgs(int argc, char** argv) {
+  AssertCond(argc == 4, "Wrong number of arguments");
+}
+
+int main(int argc, char** argv) {
+  ParseArgs(argc, argv);
+  int port = atoi(argv[1]);
+  int cols = atoi(argv[2]);
+  int rows = atoi(argv[3]);
+  const char hostname[] = "localhost";
+  std::unique_ptr<SocketClient> client_ptr(new SocketClient(hostname, port));
+  client_ptr->ConnectToServer();
+  client_ptr->SendImageDims(cols, rows);
+  // while (1) {
+  //   cv::Mat image;
+  //   GenerateImage(cols, rows, image);
+  //   client_ptr->SendImage(image);
+  // }
+
+  cv::Mat image;
+  GenerateImage(cols, rows, image);
+  client_ptr->SendImage(image);
+  
+  return 1; // Should not return
+}
