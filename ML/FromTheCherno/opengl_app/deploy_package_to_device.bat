f47f3a6a8ebef0fb0a78bdfc8483f4f3ed5ff752
Shreyas Chandra Sekhar <cshreyastech@gmail.com>
2023-07-02 12:21:03 -0700

    10-Dealing with Errors in OpenGL - ML

--- ML/FromTheCherno/opengl_app/deploy_package_to_device.bat
+++ ML/FromTheCherno/opengl_app/deploy_package_to_device.bat
@@ -1,10 +1,10 @@
 @echo off
 rem cls
 
+rem mabu opengl_app.package -t device
 
-rem mldb connect 192.168.86.247:5555
-mabu opengl_app.package -t device
-mldb uninstall opengl_app
+mldb connect 192.168.86.21:5555
+mldb uninstall com.sample.opengl_app
 mldb install -u .out/opengl_app/opengl_app.mpk
 mldb launch com.sample.opengl_app
 rem mldb terminate com.sample.opengl_app
--- ML/FromTheCherno/opengl_app/src/Application.cpp
+++ ML/FromTheCherno/opengl_app/src/Application.cpp
@@ -139,9 +139,37 @@ static void on_pause(void* user_data) {
 static void on_resume(void* user_data) {
   ML_LOG_TAG(Info, APP_TAG, "Lifecycle call on_resume()");
 }
-
-
 // -----------------------------------------------------------------------------
+
+// #define ASSERT(x) if (!(x)) __debugbreak();
+// #define GLCall(x) GLClearError();\
+//   x;\
+//   ASSERT(GLLogCall(#x, __FILE__, __LINE__))
+
+
+#define ASSERT(x) if(!(x)) raise(SIGTRAP);
+#define GLCall(x) GLClearError();\
+  x;\
+  ASSERT(GLLogCall(#x, __FILE__, __LINE__))
+
+static void GLClearError()
+{
+  while (glGetError() != GL_NO_ERROR);
+}
+
+static bool GLLogCall(const char* function, const char* file, int line)
+{
+  while (GLenum error = glGetError())
+  {
+    // std::cout << "[OpenGL_Error] (" << error << ")" << function <<
+    //   " " << file << ":" << line << std::endl;
+    ML_LOG_TAG(Error, APP_TAG, "[OpenGL_Error] ( %d ) %s %s:%d", 
+      error, function, file, line);
+    return false;
+  }
+  return true;
+}
+
 struct ShaderProgramSource
 {
   std::string VertexSource;
@@ -179,20 +207,20 @@ static ShaderProgramSource ParseShader(const std::string& filepath)
 
 static unsigned int CompileShader(unsigned int type, const std::string& source)
 {
-  unsigned int id = glCreateShader(type);
+  GLCall(unsigned int id = glCreateShader(type));
   const char* src = source.c_str();
-  glShaderSource(id, 1, &src, nullptr);
-  glCompileShader(id);
+  GLCall(glShaderSource(id, 1, &src, nullptr));
+  GLCall(glCompileShader(id));
 
   int result;
-  glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+  GLCall(glGetShaderiv(id, GL_COMPILE_STATUS, &result));
   if (result == GL_FALSE)
   {
     int length;
-    glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+    GLCall(glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length));
     // Stack side allocation of dynamic memory
     char* message = (char*)alloca(length * sizeof(char));
-    glGetShaderInfoLog(id, length, &length, message);
+    GLCall(glGetShaderInfoLog(id, length, &length, message));
     // std::cout << "Failed to compile " <<
     //   (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << " shader!" <<
     //   std::endl;
@@ -201,7 +229,7 @@ static unsigned int CompileShader(unsigned int type, const std::string& source)
     ML_LOG_TAG(Error, "Failed to compile %s shader!", 
       (type == GL_VERTEX_SHADER ? "vertex" : "fragment"));
 
-    glDeleteShader(id);
+    GLCall(glDeleteShader(id));
     return 0;
 
   }
@@ -210,17 +238,17 @@ static unsigned int CompileShader(unsigned int type, const std::string& source)
 
 static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
 {
-  unsigned int program = glCreateProgram();
+  GLCall(unsigned int program = glCreateProgram());
   unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
   unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
 
-  glAttachShader(program, vs);
-  glAttachShader(program, fs);
-  glLinkProgram(program);
-  glValidateProgram(program);
+  GLCall(glAttachShader(program, vs));
+  GLCall(glAttachShader(program, fs));
+  GLCall(glLinkProgram(program));
+  GLCall(glValidateProgram(program));
 
-  glDeleteShader(vs);
-  glDeleteShader(fs);
+  GLCall(glDeleteShader(vs));
+  GLCall(glDeleteShader(fs));
 
   return program;
 }
@@ -312,23 +340,23 @@ int main() {
   };
 
   unsigned int buffer;
-  glGenBuffers(1, &buffer);
-  glBindBuffer(GL_ARRAY_BUFFER, buffer);
-  glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
+  GLCall(glGenBuffers(1, &buffer));
+  GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));
+  GLCall(glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW));
 
   glEnableVertexAttribArray(0);
   glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
   
 
   unsigned int ibo;
-  glGenBuffers(1, &ibo);
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
+  GLCall(glGenBuffers(1, &ibo));
+  GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
+  GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW));
 
   ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
   
   unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
-  glUseProgram(shader);
+  GLCall(glUseProgram(shader));
 
   // The main/game loop
   ML_LOG_TAG(Debug, APP_TAG, "Enter main loop");
@@ -355,25 +383,25 @@ int main() {
     if (frame_result == MLResult_Ok) {
       // Prepare rendering for each camera/eye
       for (int camera = 0; camera < 2; ++camera) {
-        glBindFramebuffer(GL_FRAMEBUFFER, graphics_context.framebuffer_id);
-        glFramebufferTextureLayer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, virtual_camera_array.color_id, 0, camera);
-        glFramebufferTextureLayer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, virtual_camera_array.depth_id, 0, camera);
+        GLCall(glBindFramebuffer(GL_FRAMEBUFFER, graphics_context.framebuffer_id));
+        GLCall(glFramebufferTextureLayer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, virtual_camera_array.color_id, 0, camera));
+        GLCall(glFramebufferTextureLayer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, virtual_camera_array.depth_id, 0, camera));
 
         const MLRectf& viewport = virtual_camera_array.viewport;
-        glViewport((GLint)viewport.x, (GLint)viewport.y, (GLsizei)viewport.w, (GLsizei)viewport.h);
+        GLCall(glViewport((GLint)viewport.x, (GLint)viewport.y, (GLsizei)viewport.w, (GLsizei)viewport.h));
 
-        glClearColor(0.0, 0.0, 0.0, 0.0);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        GLCall(glClearColor(0.0, 0.0, 0.0, 0.0));
+        GLCall(glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT));
 
         // Part 2: Get the projection matrix
         MLGraphicsVirtualCameraInfo &current_camera = virtual_camera_array.virtual_cameras[camera];
         glm::mat4 projectionMatrix = rb_projection_matrix(current_camera) * rb_camera_matrix(current_camera);
 
         // Part 2: Render the object
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+        GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
 
         // Bind the frame buffer
-        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+        GLCall(glBindFramebuffer(GL_FRAMEBUFFER, 0));
         MLGraphicsSignalSyncObjectGL(graphics_client, virtual_camera_array.virtual_cameras[camera].sync_object);
       }
 
@@ -395,7 +423,7 @@ int main() {
 
   graphics_context.unmakeCurrent();
 
-  glDeleteFramebuffers(1, &graphics_context.framebuffer_id);
+  GLCall(glDeleteFramebuffers(1, &graphics_context.framebuffer_id));
 
   MLGraphicsDestroyClient(&graphics_client);
 
