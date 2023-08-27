c5f6b17a77e109cc8df666acac508e4e41076d42
Shreyas Chandra Sekhar <cshreyastech@gmail.com>
2023-08-27 05:07:51 -0700

    Transfer snappy cloud data over network

---
+++ ROSMLDesktop/networking4/client/include/client/socket_client.hpp
@@ -0,0 +1,36 @@
+#ifndef SOCKET_CLIENT_HPP
+#define SOCKET_CLIENT_HPP
+
+#include <string>
+#include <sstream>
+#include <cstdio>
+#include <cstring>
+#include <cctype> // isdigit
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <vector>
+
+#include "snappy-internal.h"
+#include "snappy-sinksource.h"
+#include "snappy.h"
+
+class SocketClient {
+ public:
+  SocketClient(const char*, int);
+  ~SocketClient();
+  void ConnectToServer();
+  void SendInt(const int);
+  void SendCloud(char[], const int);
+  
+ private:
+  void SerializeInt(const int*, unsigned char*);
+  void SerializeFloat(const float*, unsigned char*);
+
+  const char* hostname_;
+  int port_;
+  int pic_num_;
+  int sock_fdesc_conn_;
+  const int n_eye_tracking_{0};
+};
+
+#endif
