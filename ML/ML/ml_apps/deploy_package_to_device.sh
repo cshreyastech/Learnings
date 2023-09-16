0fbeb99c473541577c4c46e8fae06516d0002075
Shreyas Chandra Sekhar <cshreyastech@gmail.com>
2023-09-15 19:12:04 -0700

    Working on publishing cloud with asio and snappy.

--- ML/ML/ml_apps/deploy_package_to_device.sh
+++ ML/ML/ml_apps/deploy_package_to_device.sh
@@ -8,5 +8,6 @@ python3 build.py
 mldb connect 192.168.86.44:5555
 mldb uninstall com.magicleap.capi.sample.ros_ml_client
 mldb install -u BUILD/com.magicleap.capi.sample.ros_ml_client.mpk
+
 # mldb launch com.magicleap.capi.sample.ros_ml_client
 # mldb terminate com.magicleap.capi.sample.ros_ml_client
