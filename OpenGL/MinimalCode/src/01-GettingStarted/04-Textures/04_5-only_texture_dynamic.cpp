41fba0fd111c2bf236099574b0f31a065a64b066
Shreyas Chandra Sekhar <cshreyastech@gmail.com>
2023-11-29 14:03:47 -0800

    Added Texture dynamic

--- OpenGL/MinimalCode/src/01-GettingStarted/04-Textures/04_5-only_texture_dynamic.cpp
+++ OpenGL/MinimalCode/src/01-GettingStarted/04-Textures/04_5-only_texture_dynamic.cpp
@@ -137,23 +137,6 @@ int main()
   // set texture filtering parameters
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-  // load image, create texture and generate mipmaps
-  // int width, height, nrChannels;
-  // // The FileSystem::getPath(...) is part of the GitHub repository so we can find files on any IDE/platform; replace it with your own image path.
-  // // unsigned char *data = stbi_load(FileSystem::getPath("../src/res/texture/container.jpg").c_str(), &width, &height, &nrChannels, 0);
-  // unsigned char *data = stbi_load("../src/res/texture/container.jpg", &width, &height, &nrChannels, 0);
-
-
-  // if (data)
-  // {
-  //   glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-  //   glGenerateMipmap(GL_TEXTURE_2D);
-  // }
-  // else
-  // {
-  //     std::cout << "Failed to load texture" << std::endl;
-  // }
-  // stbi_image_free(data);
 
   int i = 0;
   std::string texture_path = "../src/res/texture/container.jpg";
