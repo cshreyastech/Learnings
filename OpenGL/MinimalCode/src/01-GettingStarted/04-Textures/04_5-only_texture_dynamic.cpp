ad216cf7b493f3e7c8387048960037b26a156b39
Shreyas Chandra Sekhar <cshreyastech@gmail.com>
2024-01-20 11:56:50 -0800

    Clean up.

--- OpenGL/MinimalCode/src/01-GettingStarted/04-Textures/04_5-only_texture_dynamic.cpp
+++ OpenGL/MinimalCode/src/01-GettingStarted/04-Textures/04_5-only_texture_dynamic.cpp
@@ -91,21 +91,6 @@ int main()
 
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
-  /***
-  // glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-  // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-  // glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-
-  // // position attribute
-  // glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-  // glEnableVertexAttribArray(0);
-  // // texture coord attribute
-  // glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-  // glEnableVertexAttribArray(1);
-  ***/
-
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), nullptr, GL_DYNAMIC_DRAW);
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
   glEnableVertexAttribArray(0);
