f7f37d207f7143ba1db30da62ef0bf08c8a6aed3
Shreyas Chandra Sekhar <cshreyastech@gmail.com>
2024-09-29 10:42:47 -0700

    Clean up.

--- OpenGL/OpenGL-1/OpenGL-Core/vendor/spdlog/out/build/x64-Debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/CS/GameEngine/cshreyastech/OpenGL-1/OpenGL-Core/vendor/spdlog
-# Build directory: C:/CS/GameEngine/cshreyastech/OpenGL-1/OpenGL-Core/vendor/spdlog/out/build/x64-Debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("example")
-subdirs("tests")
